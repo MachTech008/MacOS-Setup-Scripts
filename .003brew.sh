b0d754d43adf3501c060e9a035aeab44d4baaebc
Aheri Stanford-Asiyo <MachTech008@users.noreply.github.com>
2017-01-04 15:17:31 -0800

    Update oh-my-zsh installation instructions
    ZSH and oh-my-zsh will no longer rely on Homebrew, reducing errors

--- .003brew.sh
+++ .003brew.sh
@@ -5,11 +5,10 @@ echo "STARTING '003brew.sh' script"
 
 
 
-
-
 ########################
 # START Install Homebrew
 ########################
+
 # Before beginning, ask for the administrator password upfront
 sudo -v
 
@@ -30,39 +29,21 @@ else
 fi
 #######################
 # END Install Homebrew
-######################
+#######################
 
 
-
-
-
-
-#########################################
+########################################
 # START Install my Homebrew packages
 ########################################
+
 # Make sure we’re using the latest Homebrew.
 brew update
 
 # Upgrade any already-installed formulae.
 brew upgrade
 
-# download zsh shell with plugins
-brew install zsh
-brew install oh-my-zsh
-# Additional completion definitions for Zsh.
-git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
-# Shows your current command as green(valid command) or red(invalid command)
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/custom/plugins/zsh-syntax-highlighting
-# Better NPM completions
-git clone https://github.com/lukechilds/zsh-better-npm-completion ~/.oh-my-zsh/custom/plugins/zsh-better-npm-completion
-
-# If you're still in the default shell, change default shell to zsh manually
-sh -c "echo '/usr/local/bin/zsh' >> /etc/shells" && chsh -s /usr/local/bin/zsh
-
-
-
 # Install font tools.
-brew tap caskroom/fonts         #installs Fira Code font with Ligatures see: https://github.com/tonsky/FiraCode/wiki
+brew tap caskroom/fonts               # installs Fira Code font w/ Ligatures (https://github.com/tonsky/FiraCode/wiki)
 brew cask install font-fira-code
 
 # Install other useful binaries.
@@ -70,8 +51,6 @@ brew install tree                     # http://brewformulas.org/tree
 brew install git                      # http://brewformulas.org/Git
 brew install shellcheck               # used for Atom linter shellcheck
 brew install sqlformat                # used for Atom Beautify package
-
-
 
 # Type `git open` to open the GitHub page or website for a repository.
 npm install -g git-open
@@ -81,11 +60,34 @@ npm install -g git-recent
 
 # Remove outdated versions from the cellar.
 brew cleanup
-#########################################
+#####################################
 # END Install of my Homebrew packages
+#####################################
+
+
+
+########################################
+# START Install of ZSH SHELL and plugins
 ########################################
 
+# Add zsh shell and oh-my-zsh for awesometastic shell use
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# ZSH Plugins:
+
+# Add zsh plugin for more completion definitions
+git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+# Add zsh plugin to show current command as green (valid) or red (invalid)
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/custom/plugins/zsh-syntax-highlighting
+# Add zsh plugin for better NPM completions
+git clone https://github.com/lukechilds/zsh-better-npm-completion ~/.oh-my-zsh/custom/plugins/zsh-better-npm-completion
+
+# If you're still in the default shell, change default shell to zsh manually
+sh -c "echo '/usr/local/bin/zsh' >> /etc/shells" && chsh -s /usr/local/bin/zsh
+
+########################################
+# END Install of ZSH SHELL and plugins
+########################################
 
 
 
